b15387d0b39cc4d132ad2a68070d43030a4390c2
madarp <madarp@pobox.com>
2018-08-05 17:30:31 -0400

    Added dotm_search.py placeholder file

---
+++ dotm_search.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""
+Given a directory path, this searches all files in the path for a given text string 
+within the 'word/document.xml' section of a MSWord .dotm file.
+"""
+
+# Your awesome code begins here!
