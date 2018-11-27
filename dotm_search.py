02697b19c16b102ea1d5f1c7ecb1825d6031828f
Piero Madar <madarp@pobox.com>
2018-11-27 13:35:51 -0500

    starter code tweaks

--- dotm_search.py
+++ dotm_search.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
-Given a directory path, this searches all files in the path for a given text string 
+Given a directory path, search all files in the path for a given text string
 within the 'word/document.xml' section of a MSWord .dotm file.
 """
+__author__ = "???"
 
-# Your awesome code begins here!
+
+def main():
+    raise NotImplementedError("Your awesome code begins here!")
+
+
+if __name__ == '__main__':
+    main()
